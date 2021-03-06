61882177553314ad5328570e5fcd978ed7e70e7c
letiiciia <leticialimaferreira2@gmail.com>
2021-03-05 22:22:01 -0300

    exercises

---
+++ exercises.js
@@ -0,0 +1,124 @@
+/*
+Declare uma constante `pessoa`, que receba suas informações pessoais.
+As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
+- `nome` - String
+- `sobrenome` - String
+- `idade` - Number
+- `altura` - Number
+- `peso` - Number
+- `andando` - Boolean - iniciado com "false"
+- `caminhouQuantosMetros` - Number - iniciado com zero
+*/
+
+
+
+/*
+Adicione uma função ao objeto `pessoa` chamado `fazerAniversario`. A função deve
+alterar o valor da propriedade `idade` dessa pessoa, somando `1` a cada vez que
+for chamado.
+*/
+
+
+
+/*
+Adicione um método ao objeto `pessoa` chamado `andar`, que terá as seguintes
+características:
+- Esse método deve receber por parâmetro um valor que representará a quantidade
+de metros caminhados;
+- Ele deve alterar o valor da propriedade `caminhouQuantosMetros`, somando ao
+valor dessa propriedade a quantidade passada por parâmetro;
+- Ele deverá modificar o valor da propriedade `andando` para o valor
+booleano que representa "verdadeiro";
+*/
+
+
+
+/*
+Adicione um método ao objeto `pessoa` chamado `parar`, que irá modificar o valor
+da propriedade `andando` para o valor booleano que representa "falso".
+*/
+
+
+
+ /*
+Crie um método chamado `nomeCompleto`, que retorne a frase:
+- "Olá! Meu nome é [NOME] [SOBRENOME]!"
+*/
+
+
+
+
+/*
+Crie um método chamado `mostrarIdade`, que retorne a frase:
+- "Olá, eu tenho [IDADE] anos!"
+*/
+
+
+
+/*
+Crie um método chamado `mostrarPeso`, que retorne a frase:
+- "Eu peso [PESO]Kg."
+*/
+
+
+
+/*
+Crie um método chamado `mostrarAltura` que retorne a frase:
+- "Minha altura é [ALTURA]m."
+*/
+
+
+
+
+/*
+Faça a `pessoa` fazer 3 aniversários.
+*/
+
+
+
+
+/*
+Quantos anos a `pessoa` tem agora? (Use a instrução para responder e
+comentários inline ao lado da instrução para mostrar qual foi a resposta
+retornada)
+*/
+
+
+/*
+Agora, faça a `pessoa` caminhar alguns metros, invocando o método `andar` 3x,
+com metragens diferentes passadas por parâmetro.
+*/
+
+
+
+
+/*
+Crie um método chamado `mostrarDistancia` que retorne a frase:
+- "[NOME] caminhou [caminhouQuantosMetros]m"
+*/
+
+
+
+
+
+/*
+Crie um método para o objeto `pessoa` chamado `apresentacao`. Esse método deve
+retornar a string:
+- "Olá, eu sou [NOME COMPLETO], tenho [IDADE] anos, [ALTURA], meu peso é [PESO] e, só hoje, eu já caminhei [CAMINHOU QUANTOS METROS] metros!"
+Só que, antes de retornar a string, você vai fazer algumas validações:
+- Se a idade for `1`, a frase acima, na parte que fala da idade, vai mostrar a
+palavra "ano" ao invés de "anos", pois é singular;
+- Se a quantidade de metros caminhados for igual a `1`, então a palavra que
+deve conter no retorno da frase acima é "metro" no lugar de "metros".
+- Para cada validação, você irá declarar uma variável localmente (dentro do
+método), que será concatenada com a frase de retorno, mostrando a resposta
+correta, de acordo com os dados inseridos no objeto.
+*/
+
+// "Olá, eu sou [NOME COMPLETO], tenho [IDADE] anos, [ALTURA], meu peso é [PESO] e, só hoje, eu já caminhei [CAMINHOU QUANTOS METROS] metros!"
+// Se a idade for `1`, a frase acima, na parte que fala da idade, vai mostrar a
+//palavra "ano" ao invés de "anos", pois é singular;
+// - Se a quantidade de metros caminhados for igual a `1`, então a palavra que
+// deve conter no retorno da frase acima é "metro" no lugar de "metros".
+
+
